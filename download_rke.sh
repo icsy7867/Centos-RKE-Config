d84ad128929831f5927849d7d117071febe4aa49
Rancher user account <wstewart3@gmail.com>
2020-05-01 07:45:36 -0400

    delete disabled

--- download_rke.sh
+++ download_rke.sh
@@ -4,3 +4,6 @@ echo $latest
 
 wget -O rke $latest 
 chmod +x rke
+
+
+helm=$(curl -s https://api.github.com/repos/helm/helm/releases/latest | grep "browser_download_url" | grep "linux-amd64" | cut -d : -f 2,3 | tr -d \")
